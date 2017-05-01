fafee09c2fee0731d3d661db8e81c8e1c9e8d5e0
Andrew Nicols <andrew@nicols.co.uk>
2017-05-01 11:12:39 +0800

    Reorganise code

--- run_task.sh
+++ run_task.sh
@@ -2,8 +2,6 @@
 
 set -e
 
-echo "############################################################################"
-
 # Grab the script directory.
 # We need this to form reliable paths to our libraries.
 pushd `dirname $0` > /dev/null
@@ -11,41 +9,15 @@ SCRIPTPATH=`pwd`
 popd > /dev/null
 INCPATH="${SCRIPTPATH}/inc"
 
-# Perform setup
-. "${INCPATH}/setup"
+pushd "${SCRIPTPATH}" > /dev/null
+THISVERSION=`git rev-parse HEAD`
+popd > /dev/null
 
-echo "# Running job '${UNIQID}'"
-echo "# Requested target ${TARGET} ${TARGET_ARGS}"
-echo "# Requested database ${DB} ${DBVERSION}"
-
-# Setup database
-. "${INCPATH}/database"
-
-# Setup the codebase to be tested.
-. "${INCPATH}/codebase"
-
-# Perform setup for the specified target.
-. "${INCPATH}/target"
-
-# TODO
-PHP_SERVER_DOCKER='rajeshtaneja/php:7.0'
-
-echo "# Starting ${UNIQID} run for ${TARGET} ${TARGET_ARGS}"
-echo "# Database: ${DB}"
-echo "# PHP Image: ${PHP_SERVER_DOCKER}"
+echo "############################################################################"
+echo "############################################################################"
+echo "### Starting nightly (Version ${THISVERSION})"
+echo "############################################################################"
 echo "############################################################################"
 
-cd "${TEMPDIR}"
-DOCKER_MOODLE_PATH=/var/www/html/moodle
-
-echo "# Starting docker run"
-DOCKER_RUN_ARGS_VOLS="-v ${WORKSPACE}:${DOCKER_MOODLE_PATH}:rw -v ${CACHEDIR}/composer:/.composer/cache:rw"
-DOCKER_RUN_ARGS_ENTRY="--entrypoint ${ENTRYPOINT}"
-DOCKER_RUN_ARGS_DB="--dbtype=${DBTYPE} --dbhost=${HOST_DB} --dbname=${DBNAME} --phpunitdbprefix=${DBPREFIX} --dbuser=${DBUSER} --dbpass=${DBPASS}"
-DOCKER_RUN_ARGS=" ${DOCKER_RUN_ARGS_VOLS} ${DOCKER_RUN_ARGS_ENTRY} ${PHP_SERVER_DOCKER} ${DOCKER_RUN_ARGS_DB} $EXTRA_OPT --forcedrop"
-docker run -i --rm -u `id -u $USER` -e "COMPOSER_CACHE_DIR=/.composer/cache" --name ${HOST_PHP} --network "${NETWORK}" ${DOCKER_RUN_ARGS}
-EXITCODE=$?
-
-echo "# Run completed with exit code of ${EXITCODE}"
-
-exit $EXITCODE
+# Perform setup.
+. "${INCPATH}/setup"
